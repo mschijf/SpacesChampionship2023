93f2ef14772f5d65366f5be1750fdd79e381c3d4
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-09-09 07:10:21 +0200

    generalized console to be used with data file as well

--- src/main/kotlin/scs2023/BusRide.kt
+++ src/main/kotlin/scs2023/BusRide.kt
@@ -1,47 +1,73 @@
 package scs2023
 
+import java.io.File
 import kotlin.math.absoluteValue
 
-val console = java.util.Scanner(System.`in`)
-val numberOfTests = console.nextLine().toInt()
-
 fun main() {
-    repeat(numberOfTests) {
-        testCase()
+    BusRide(useConsole = false).main()
+}
+
+class BusRide(useConsole: Boolean) {
+    private val console = getConsole(useConsole)
+    private val numberOfTests = console.nextLine().toInt()
+
+    fun main() {
+        repeat(numberOfTests) {
+            testCase()
+        }
     }
-}
 
-fun testCase() {
-    val firstLine = console.nextLine()
-    val numberOfHouses = firstLine.substringBefore(" ").toInt()
-    val inputLines = (1..numberOfHouses).map{console.nextLine()}
+    private fun testCase() {
+        val firstLine = console.nextLine()
+        val numberOfHouses = firstLine.substringBefore(" ").toInt()
+        val inputLines = (1..numberOfHouses).map { console.nextLine() }
 
-    val hq = Point.of(firstLine.substringAfter(" "))
-    val homeList = inputLines.map { Point.of(it) }
+        val hq = Point.of(firstLine.substringAfter(" "))
+        val homeList = inputLines.map { Point.of(it) }
 
-    val allocations = (homeList + hq)
+        val allocations = (homeList + hq)
 
-    println(allocations.shortestRoundTrip(from=hq))
-}
-
-
-fun List<Point>.shortestRoundTrip(from: Point, final: Point = from, pathDone: List<Point> = listOf(from), length: Int = 0): Int {
-    return if (pathDone.size == this.size) {
-        from.distanceTo(final)
-
-    } else {
-
-        this
-            .filter { location -> location !in pathDone }
-            .minOf { newLocation -> shortestRoundTrip(newLocation, final, pathDone + newLocation, length + from.distanceTo(newLocation)) + from.distanceTo(newLocation) }
+        println(allocations.shortestRoundTrip(from = hq))
     }
-}
 
-data class Point(val x: Int, val y: Int) {
 
-    fun distanceTo(other: Point) = (other.x - x).absoluteValue + (other.y - y).absoluteValue
+    private fun List<Point>.shortestRoundTrip(
+        from: Point,
+        final: Point = from,
+        pathDone: List<Point> = listOf(from),
+        length: Int = 0
+    ): Int {
+        return if (pathDone.size == this.size) {
+            from.distanceTo(final)
 
-    companion object {
-        fun of(input: String): Point = input.split("\\s+".toRegex()).run { Point(this[0].toInt(), this[1].toInt()) }
+        } else {
+
+            this
+                .filter { location -> location !in pathDone }
+                .minOf { newLocation ->
+                    shortestRoundTrip(
+                        newLocation,
+                        final,
+                        pathDone + newLocation,
+                        length + from.distanceTo(newLocation)
+                    ) + from.distanceTo(newLocation)
+                }
+        }
     }
+
+    data class Point(val x: Int, val y: Int) {
+
+        fun distanceTo(other: Point) = (other.x - x).absoluteValue + (other.y - y).absoluteValue
+
+        companion object {
+            fun of(input: String): Point = input.split("\\s+".toRegex()).run { Point(this[0].toInt(), this[1].toInt()) }
+        }
+    }
+
+    private fun getConsole(useConsole: Boolean) =
+        if (useConsole)
+            java.util.Scanner(System.`in`)
+        else
+            java.util.Scanner(File("data/${this.javaClass.name.substringAfterLast(".")}/example").bufferedReader())
 }
+
--- src/main/kotlin/scs2023/IntelliJIsBroken.kt
+++ src/main/kotlin/scs2023/IntelliJIsBroken.kt
@@ -1,30 +1,49 @@
 package scs2023
 
-val bracesMap = mapOf('{' to '}', '[' to ']', '(' to ')')
+import java.io.File
+
 
 fun main() {
-    val console = java.util.Scanner(System.`in`)
-    val firstLine = console.nextLine().toInt()
-    val stringList = (1..firstLine).map{console.nextLine()}
-
-    stringList.forEach {
-        if (it.inBalance()) println("YES") else println("NO")
-    }
+    IntelliJIsBroken(useConsole = false).main()
 }
 
-private fun String.inBalance(): Boolean {
-    val stack = ArrayDeque<Char>()
-    this.forEach {ch ->
-        when (ch) {
-            '{', '[', '(' -> {
-                stack.add(ch)
-            }
-            '}', ']', ')' -> {
-                val top = stack.removeLastOrNull()
-                if (top != null && bracesMap[top] != ch)
-                    return false
-            }
+class IntelliJIsBroken(useConsole: Boolean) {
+    private val console = getConsole(useConsole)
+
+    private val bracesMap = mapOf('{' to '}', '[' to ']', '(' to ')')
+
+    fun main() {
+
+        val firstLine = console.nextLine().toInt()
+        val stringList = (1..firstLine).map{console.nextLine()}
+
+        stringList.forEach {
+            if (it.inBalance()) println("YES") else println("NO")
         }
     }
-    return stack.isEmpty()
+
+    private fun String.inBalance(): Boolean {
+        val stack = ArrayDeque<Char>()
+        this.forEach {ch ->
+            when (ch) {
+                '{', '[', '(' -> {
+                    stack.add(ch)
+                }
+                '}', ']', ')' -> {
+                    val top = stack.removeLastOrNull()
+                    if (top != null && bracesMap[top] != ch)
+                        return false
+                }
+            }
+        }
+        return stack.isEmpty()
+    }
+
+    private fun getConsole(useConsole: Boolean) =
+        if (useConsole)
+            java.util.Scanner(System.`in`)
+        else
+            java.util.Scanner(File("data/${this.javaClass.name.substringAfterLast(".")}/example").bufferedReader())
+
 }
+
