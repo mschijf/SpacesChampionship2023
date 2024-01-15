4571964c5733b87abdbfb99d94f8cf71200e779b
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2024-01-15 11:41:12 +0100

    Space championship 2024-01 password execercise, including code to create input files

--- src/main/kotlin/scs2024/Password.kt
+++ src/main/kotlin/scs2024/Password.kt
@@ -1,9 +1,13 @@
 package scs2024
 
+import kotlin.math.absoluteValue
+import kotlin.random.Random
+
 fun main() {
     var passwordOk = 0
 
-    repeat (3) {
+    val lines = readln().toInt()
+    repeat (lines) {
         val line = readln()
         val name = line.split(" ").first()
         val password = line.split(" ").last()
@@ -12,6 +16,22 @@ fun main() {
     }
 
     println("$passwordOk")
+
+//    createPasswordFile(1000)
+}
+
+private fun createPasswordFile(lines: Int) {
+    println(lines)
+    repeat(lines) {
+        val name = createName()
+        val pw = if (Random.nextBoolean())
+            createPasswordWrong(name)
+        else {
+            val tmp = createPasswordCorrect(name)
+            if (tmp == "") createPasswordWrong(name) else tmp
+        }
+        println("$name $pw")
+    }
 }
 
 private fun checkPasswordOk(name: String, password: String) : Boolean {
@@ -26,4 +46,55 @@ private fun checkPasswordOk(name: String, password: String) : Boolean {
     }
 
     return iName < name.length
+}
+
+private fun createName() : String {
+    val length = Random.nextInt().absoluteValue % 100
+    val bld = StringBuilder()
+    repeat(length) {
+        val letter = Random.nextInt().absoluteValue % 26
+        bld.append('a' + letter)
+    }
+    return bld.toString()
+}
+
+private fun createPasswordWrong(name: String): String {
+    val length = Random.nextInt().absoluteValue % 1000
+    val bld = StringBuilder()
+    var iName = 0
+    for (i in 0 .. length) {
+        val letter = Random.nextInt().absoluteValue % 26
+        bld.append('a' + letter)
+        if (iName < name.length && Random.nextBoolean()) {
+            bld.append(name[iName])
+            iName++
+        }
+    }
+    while (iName < name.length) {
+        bld.append(name[iName])
+        iName++
+    }
+    return bld.toString()
+}
+
+private fun createPasswordCorrect(name: String): String {
+    repeat (10) {
+        val length = Random.nextInt().absoluteValue % 1000
+        val bld = StringBuilder()
+        var iName = 0
+        val nn = name.toCharArray()
+        nn.shuffle()
+        val newName = nn.joinToString("")
+        for (i in 0..length) {
+            val letter = Random.nextInt().absoluteValue % 26
+            bld.append('a' + letter)
+            if (iName < newName.length && Random.nextBoolean()) {
+                bld.append(newName[iName])
+                iName++
+            }
+        }
+        if (checkPasswordOk(name, bld.toString()))
+            return bld.toString()
+    }
+    return ""
 }
